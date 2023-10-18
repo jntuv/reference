49e5b1957770cad94473c3c1f8c90972bd151118
santosh mudiganti <36408125+jntuv@users.noreply.github.com>
2023-10-18 15:30:39 +0530

    Create amd_import_in_script.js

---
+++ amd/amd_import_in_script.js
@@ -0,0 +1,12 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Suitelet
+ * @NModuleScope SameAccount
+ * @NAMDConfig ./package.json
+ */
+
+define(['./counter','moment'], function (
+	
+  counter,
+  moment
+) {
